1c7d32b4e3f7b7ab8bdf9f296a07c5913b86b6ce
mmstfkc <36615411+mmstfkc@users.noreply.github.com>
2021-05-27 15:22:48 +0300

    İnput kısımları eklendi

--- Otomasyon/musteri_islemleri.cs
+++ Otomasyon/musteri_islemleri.cs
@@ -20,20 +20,58 @@ namespace oto_kiralama_otomasyonu
        
         private void musteri_islemleri_Load(object sender, EventArgs e)
         {
-            
+            doldur();
         }
 
-        
- 
+        void doldur()
+        {
+            try
+            {
+                
+            }
+            catch (Exception hata)
+            {
+                MessageBox.Show(hata.Message);
+            }
+          
 
-        
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            menu menu = new menu();
+            menu.Show();
+            this.Hide();
+        }
+
+     
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            maskedTextBox1.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            textBox2.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            textBox3.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            textBox4.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            textBox5.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
+            textBox6.Text = dataGridView1.CurrentRow.Cells[6].Value.ToString();
+            textBox8.Text = dataGridView1.CurrentRow.Cells[7].Value.ToString();
+            comboBox1.Text = dataGridView1.CurrentRow.Cells[8].Value.ToString();
+            textBox9.Text = dataGridView1.CurrentRow.Cells[9].Value.ToString();
+        }
 
 
 
-  
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+          
+                
+                
 
+
+
+           
        
+    }
 
-    
+
     }
 }
