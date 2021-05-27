8b18d650344ca5329eec5edf8abda263d6b6f8c3
mmstfkc <36615411+mmstfkc@users.noreply.github.com>
2021-05-27 15:18:26 +0300

    Müşteri İşlemleri Sayfası OLuşturuldu

---
+++ Otomasyon/musteri_islemleri.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace oto_kiralama_otomasyonu
+{
+    partial class musteri_islemleri
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // musteri_islemleri
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(552, 514);
+            this.Font = new System.Drawing.Font("Verdana", 8.25F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "musteri_islemleri";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Müşteri İşlemleri";
+            this.Load += new System.EventHandler(this.musteri_islemleri_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Otomasyon/musteri_islemleri.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace oto_kiralama_otomasyonu
+{
+    public partial class musteri_islemleri : Form
+    {
+        public musteri_islemleri()
+        {
+            InitializeComponent();
+        }
+        
+       
+        private void musteri_islemleri_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+ 
+
+        
+
+
+
+  
+
+       
+
+    
+    }
+}
