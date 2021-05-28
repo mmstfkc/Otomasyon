5a5a677bfa07a833a01601d0e125278f7265da26
ranakaya2289@gmail.com <ranakaya2289@gmail.com>
2021-05-28 15:20:31 +0300

    veritabanı bağlantısı yapıldı

--- Otomasyon/Arac_Kiralama.cs
+++ Otomasyon/Arac_Kiralama.cs
@@ -16,7 +16,8 @@ namespace oto_kiralama_otomasyonu
         {
             InitializeComponent();
         }
-        
+        SqlConnection baglanti = new SqlConnection("Data Source=localhost\\SQLExpress; initial Catalog=oto_kiralama; Integrated Security=true");
+        DataTable dt = new DataTable();
         private void Arac_Kiralama_Load(object sender, EventArgs e)
         {
             musteridoldur();
@@ -27,6 +28,32 @@ namespace oto_kiralama_otomasyonu
         {
             try
             {
+                if (baglanti.State == ConnectionState.Closed)
+                {
+                    baglanti.Open();
+                }
+                dt.Clear();
+                SqlDataAdapter listele = new SqlDataAdapter("select * from kiralama", baglanti);
+                listele.Fill(dt);
+                dataGridView1.DataSource = dt;
+                listele.Dispose();
+                dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+                dataGridView1.Columns[0].Visible = false;
+                dataGridView1.RowHeadersVisible = false;
+                dataGridView1.Columns[1].Width = 80;
+                dataGridView1.Columns[2].Width = 120;
+                dataGridView1.Columns[3].Width = 77;
+                dataGridView1.Columns[4].Width = 76;
+                dataGridView1.Columns[5].Width = 76;
+             
+                baglanti.Close();
+             
+                comboBox2.Text = "";
+                textBox3.Text = "";
+                textBox4.Text = "";
+                textBox5.Text = "";
+          
+                comboBox1.Text = "";
             }
             catch (Exception hata)
             {
@@ -35,11 +62,33 @@ namespace oto_kiralama_otomasyonu
         }
         void musteridoldur()
         {
-            
+            comboBox1.Items.Clear();
+            if (baglanti.State == ConnectionState.Closed)
+            {
+                baglanti.Open();
+            }
+            SqlCommand komut = new SqlCommand("select tc from musteri",baglanti);
+            SqlDataReader oku = komut.ExecuteReader();
+            while (oku.Read())
+            {
+                comboBox1.Items.Add(oku["tc"].ToString());
+            }
+            baglanti.Close();
         }
         void uygunaracdoldur()
         {
-            
+            comboBox2.Items.Clear();
+            if (baglanti.State == ConnectionState.Closed)
+            {
+                baglanti.Open();
+            }
+            SqlCommand komut = new SqlCommand("select plaka from arac where durum='"+ "Uygun" + "'", baglanti);
+            SqlDataReader oku = komut.ExecuteReader();
+            while (oku.Read())
+            {
+                comboBox2.Items.Add(oku["plaka"].ToString());
+            }
+            baglanti.Close();
         }
        
 
@@ -60,16 +109,45 @@ namespace oto_kiralama_otomasyonu
             textBox4.Text = dateTimePicker2.Value.ToShortDateString();
         }
 
-       
+        
 
    
+
         private void textBox7_TextChanged(object sender, EventArgs e)
         {
-            
+            if (baglanti.State==ConnectionState.Closed)
+            {
+                baglanti.Open();
+            }
+            dt.Clear();
+
+            SqlDataAdapter adtr = new SqlDataAdapter("select * From kiralama where tc like'%" + textBox7.Text + "%'or plaka like '%" + textBox7.Text + "%'or alis_tarihi like '%" + textBox7.Text + "%'or veris_tarihi like '%" + textBox7.Text + "%'or ucret like '%" + textBox7.Text + "%'", baglanti);
+            adtr.Fill(dt);
+            dataGridView1.DataSource = dt;
+            adtr.Dispose();
+
+
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.Columns[0].Visible = false;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.Columns[1].Width = 80;
+            dataGridView1.Columns[2].Width = 120;
+            dataGridView1.Columns[3].Width = 77;
+            dataGridView1.Columns[4].Width = 76;
+            dataGridView1.Columns[5].Width = 76;
+
+            baglanti.Close();
+
+            comboBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+    
+            comboBox1.Text = "";
 
         }
 
-        
+      
 
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
