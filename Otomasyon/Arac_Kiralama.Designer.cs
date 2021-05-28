b2e99f17b5ee7e879f79100458ac872c7c05a794
ranakaya2289@gmail.com <ranakaya2289@gmail.com>
2021-05-28 15:05:59 +0300

    araç kiralama paneli oluşturuldu

---
+++ Otomasyon/Arac_Kiralama.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace oto_kiralama_otomasyonu
+{
+    partial class Arac_Kiralama
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Arac_Kiralama
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(522, 427);
+            this.Font = new System.Drawing.Font("Verdana", 8.25F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Arac_Kiralama";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Araç Kiralama Paneli";
+            this.Load += new System.EventHandler(this.Arac_Kiralama_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Otomasyon/Arac_Kiralama.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace oto_kiralama_otomasyonu
+{
+    public partial class Arac_Kiralama : Form
+    {
+        public Arac_Kiralama()
+        {
+            InitializeComponent();
+        }
+       
+        private void Arac_Kiralama_Load(object sender, EventArgs e)
+        {
+          
+        }
+               
+    }
+}
