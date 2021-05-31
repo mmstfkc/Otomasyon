2e285fe536e8ee6196e28eeb364965e43ef27e93
ranakaya2289@gmail.com <ranakaya2289@gmail.com>
2021-05-31 18:12:53 +0300

    3.5.a

---
+++ Otomasyon/arac_satis.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace oto_kiralama_otomasyonu
+{
+    partial class arac_satis
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // arac_satis
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(636, 388);
+            this.Font = new System.Drawing.Font("Verdana", 8.25F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "arac_satis";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Araç Satış";
+            this.Load += new System.EventHandler(this.arac_satis_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Otomasyon/arac_satis.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace oto_kiralama_otomasyonu
+{
+    public partial class arac_satis : Form
+    {
+        public arac_satis()
+        {
+            InitializeComponent();
+        }
+       
+        private void arac_satis_Load(object sender, EventArgs e)
+        {
+           
+        }
+        
+        
+        
+    }
+}
