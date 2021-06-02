cd18e1cc2a13fe8ad13dab7b06cc53ff14884df1
mmstfkc <36615411+mmstfkc@users.noreply.github.com>
2021-06-02 20:44:20 +0300

    Case 3.4.b tamamlandı

--- Otomasyon/arac_Teslim.Designer.cs
+++ Otomasyon/arac_Teslim.Designer.cs
@@ -28,7 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Teslim Alınacak Aracın Plakası :";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(206, 59);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(121)))), ((int)(((byte)(107)))));
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(12, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 25;
+            this.button5.Text = "Anasayfa";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // arac_Teslim
             // 
@@ -36,6 +69,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(349, 140);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Verdana", 8.25F);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "arac_Teslim";
@@ -43,9 +79,14 @@
             this.Text = "Araç Teslim Alınması";
             this.Load += new System.EventHandler(this.arac_Teslim_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button5;
     }
 }
--- Otomasyon/arac_Teslim.cs
+++ Otomasyon/arac_Teslim.cs
@@ -12,20 +12,29 @@ namespace oto_kiralama_otomasyonu
 {
     public partial class arac_Teslim : Form
     {
-
+        
         public arac_Teslim()
         {
             InitializeComponent();
         }
 
- 
-        
+        private void button5_Click(object sender, EventArgs e)
+        {
+            menu menu = new menu();
+            menu.Show();
+            this.Hide();
+        }
+        void uygunaracdoldur()
+        {
+            comboBox1.Items.Clear();
+            
+        }
 
         
 
         private void arac_Teslim_Load(object sender, EventArgs e)
         {
-            
+            uygunaracdoldur();
         }
     }
 }
