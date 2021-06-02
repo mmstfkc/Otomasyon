993fa8ab8e628929cd460f51f690d9b633286150
ranakaya2289@gmail.com <ranakaya2289@gmail.com>
2021-06-02 15:32:26 +0300

    3.5.b

--- Otomasyon/arac_satis.cs
+++ Otomasyon/arac_satis.cs
@@ -18,10 +18,83 @@ namespace oto_kiralama_otomasyonu
        
         private void arac_satis_Load(object sender, EventArgs e)
         {
+            musteridoldur();
+            uygunaracdoldur();
+            doldur();
+        }
+        void uygunaracdoldur()
+        {
+            comboBox2.Items.Clear();
+            
+        }
+        void musteridoldur()
+        {
+            comboBox1.Items.Clear();
            
         }
-        
-        
-        
+        void doldur()
+        {
+            try
+            {
+               
+                
+            }
+            catch (Exception hata)
+            {
+                MessageBox.Show(hata.Message);
+            }
+        }
+
+       
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+            
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            textBox3.Text = dateTimePicker1.Value.ToShortDateString();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            textBox4.Text = dateTimePicker2.Value.ToShortDateString();
+            
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            comboBox1.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            comboBox2.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            textBox3.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            textBox4.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            textBox5.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            menu menu = new menu();
+            menu.Show();
+            this.Hide();
+        }    
+               
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
